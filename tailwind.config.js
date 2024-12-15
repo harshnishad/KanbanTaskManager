1e850a9795e7aec4f466e6cb96b6d3dc589e9adf
harshnishad <harsh.nishad_cs21@gla.ac.in>
2024-12-15 18:25:07 +0530

    fixed:tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
-module.exports = {
-  content: [
-    './src/**/*.{html,js,jsx,ts,tsx}', // Ensure this is pointing to the correct folder and file types
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+export const content = [
+  './src/**/*.{html,js,jsx,ts,tsx}', // Ensure this is pointing to the correct folder and file types
+];
+export const theme = {
+  extend: {},
+};
+export const plugins = [];
